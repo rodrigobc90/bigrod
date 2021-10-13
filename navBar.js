e91d3c0da356064581aec71bf4a652d96bd0d31f
rodrigobc90 <76399272+rodrigobc90@users.noreply.github.com>
2021-10-12 21:20:17 -0300

    Add files via upload

---
+++ navBar.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <>
+           <Nav>
+            <NavLogo to="/">
+                Logo
+            </NavLogo>
+            <Bars />
+
+            <NavMenu>
+                <NavLink to="/" activeStyle>
+                    Home
+                </NavLink>
+                <NavLink to="/about" activeStyle>
+                    About
+                </NavLink>
+                <NavLink to="/contact" activeStyle>
+                    Contact
+                </NavLink>
+            </NavMenu> 
+           </Nav> 
+        </>
+    );
+};
+export default Navbar;
